63ac3ac3886239c909ca5b4cfbf4c2a2c1af24d8
Jeremy Cowgar <jeremy@cowgar.com>
2020-01-20 09:00:56 -0500

    0.9.0

---
+++ lib/src/profiler.dart
@@ -0,0 +1,54 @@
+import 'section.dart';
+
+class Profiler {
+  final Map<String, Section> _sections;
+  final List<String> _nestedSections;
+  final List<String> _sectionOrder;
+
+  String _currentSectionName;
+
+  Profiler()
+      : _sections = {},
+        _nestedSections = [],
+        _sectionOrder = [];
+
+  void act() {
+    _sections[_currentSectionName].act();
+  }
+
+  void begin(String sectionName) {
+    if (_currentSectionName != null) {
+      _sections[_currentSectionName].stop();
+    }
+
+    if (!_sections.containsKey(sectionName)) {
+      _sections[sectionName] = Section(sectionName);
+      _sectionOrder.add(sectionName);
+    }
+
+    _currentSectionName = sectionName;
+    _sections[sectionName].start();
+  }
+
+  void end() {
+    _sections[_currentSectionName].stop();
+  }
+
+  void push(String sectionName) {
+    _nestedSections.add(sectionName);
+
+    begin(sectionName);
+  }
+
+  void pop() {
+    final sectionName = _nestedSections.removeLast();
+
+    _sections[sectionName].stop();
+
+    if (_nestedSections.isNotEmpty) {
+      begin(_nestedSections.last);
+    }
+  }
+
+  Iterable<Section> report() => _sectionOrder.map((s) => _sections[s]);
+}
